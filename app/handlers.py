197efbc02234550d07727232973781e88a6c0d62
Simon Polshkov <polshsem@gmail.com>
2025-04-09 23:57:00 +0300

    1.01

--- app/handlers.py
+++ app/handlers.py
@@ -6,13 +6,153 @@ import app.keyboards as kb
 
 router = Router()
 
+ # роутер для старта
 @router.message(CommandStart())
 async def start(message: Message):
     await message.answer_photo(
         photo = 'AgACAgIAAxkBAAMGZ_W9HvQRnVGYwJ_1jFto_W2JsS8AAoz3MRv7bahLrqQEdRvoU1IBAAMCAANtAAM2BA',
-        caption = 'текст про компанию и тп...',
+        caption = '',
     reply_markup = kb.main_menu
         )
+
+# кнопка прайс
+@router.callback_query(F.data == 'price')
+async def contacts(callback: CallbackQuery):
+    await callback.answer('Вы перешли в раздел прайс.')
+    if callback.message.photo:
+        await callback.message.edit_caption(
+            caption='Прайс наших картингов:',
+            reply_markup = kb.catalog
+        )
+    else:
+        await callback.message.edit_text(
+            text='Прайс наших картингов:',
+            reply_markup = kb.catalog
+        )
+# кнопка назад из прайса
+@router.callback_query(F.data == 'back_price')
+async def cart(callback: CallbackQuery):
+    await callback.answer('Вы вернулись назад.')
+    await callback.message.edit_caption(
+        caption='',
+        reply_markup=kb.main_menu
+    )
+
+# кнопка инфо
+@router.callback_query(F.data == 'info')
+async def info(callback: CallbackQuery):
+    await callback.answer('Вы перешли во вкладку информация.')
+    if callback.message.photo:
+        await callback.message.edit_caption(
+            caption='Текст по информации...',
+            reply_markup=kb.info
+        )
+    else:
+        await callback.message.edit_text(
+            text='Текст по информации...',
+            reply_markup=kb.info
+        )
+# кнопка назад из инфо
+@router.callback_query(F.data == 'back_info')
+async def cart(callback: CallbackQuery):
+    await callback.answer('Вы вернулись назад.')
+    await callback.message.edit_caption(
+        caption='',
+        reply_markup=kb.main_menu
+    )
+
+# кнопка отзывы
+@router.callback_query(F.data == 'feedback')
+async def contacts(callback: CallbackQuery):
+    await callback.answer('Вы перешли в раздел отзывы.')
+    if callback.message.photo:
+        await callback.message.edit_caption(
+            caption='Телеграм канал с отзывами:',
+            reply_markup = kb.feedback
+        )
+    else:
+        await callback.message.edit_text(
+            text='Телеграм канал с отзывами:',
+            reply_markup = kb.feedback
+        )
+# кнопка назад из отзывов
+@router.callback_query(F.data == 'back_feedback')
+async def cart(callback: CallbackQuery):
+    await callback.answer('Вы вернулись назад.')
+    await callback.message.edit_caption(
+        caption='',
+        reply_markup=kb.main_menu
+    )
+
+# кнопка связь с менеджером
+@router.callback_query(F.data == 'communication')
+async def info(callback: CallbackQuery):
+    await callback.answer('Вы перешли во вкладку связь с менеджером.')
+    if callback.message.photo:
+        await callback.message.edit_caption(
+            caption=' Связаться с нами: \n Номер телефона: +7 800 555 35 35',
+            reply_markup=kb.communication
+        )
+    else:
+        await callback.message.edit_text(
+            text=' Связаться с нами: \n Номер телефона: +7 800 555 35 35',
+            reply_markup=kb.communication
+        )
+# назад из связи
+@router.callback_query(F.data == 'back_communication')
+async def cart(callback: CallbackQuery):
+    await callback.answer('Вы вернулись назад.')
+    await callback.message.edit_caption(
+        caption='',
+        reply_markup=kb.main_menu
+    )
+
+# кнопка карт1
+@router.callback_query(F.data == 'cart1')
+async def info(callback: CallbackQuery):
+    await callback.answer('Вы перешли во вкладку карт1.')
+    if callback.message.photo:
+        await callback.message.edit_caption(
+            caption='карт1',
+            reply_markup=kb.cart_order
+        )
+    else:
+        await callback.message.edit_text(
+            text='карт1',
+            reply_markup=kb.cart_order
+        )
+# назад из карт1
+@router.callback_query(F.data == 'back_cart_order')
+async def cart(callback: CallbackQuery):
+    await callback.answer('Вы вернулись назад.')
+    await callback.message.edit_caption(
+        caption='',
+        reply_markup=kb.catalog
+    )
+
+
+# связь с менеджером для вкладки карт1
+@router.callback_query(F.data == 'communication_order')
+async def info_order(callback: CallbackQuery):
+    await callback.answer('Вы перешли во вкладку связь с менеджером.')
+    if callback.message.photo:
+        await callback.message.edit_caption(
+            caption=' Связаться с нами: \n Номер телефона: +7 800 555 35 35',
+            reply_markup=kb.communication_order
+        )
+    else:
+        await callback.message.edit_text(
+            text=' Связаться с нами: \n Номер телефона: +7 800 555 35 35',
+            reply_markup=kb.communication_order
+        )
+# назад из связи с менеджером карт1
+@router.callback_query(F.data == 'back_communication_order')
+async def cart(callback: CallbackQuery):
+    await callback.answer('Вы вернулись назад.')
+    await callback.message.edit_caption(
+        caption='',
+        reply_markup=kb.cart_order
+    )
 
 '''
 @router.message(F.photo)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,12 +1,50 @@
 from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup)
 
+# клавиатура основного меню
 main_menu = InlineKeyboardMarkup(inline_keyboard = [
-    [InlineKeyboardButton(text = 'Цены', callback_data = 'cash')],
+    [InlineKeyboardButton(text = 'Прайс', callback_data = 'price')],
+    [InlineKeyboardButton(text = 'Инфо', callback_data = 'info')],
     [InlineKeyboardButton(text = 'Отзывы', callback_data = 'feedback')],
-    [InlineKeyboardButton(text = 'О нас', callback_data = 'about_us')]
+    [InlineKeyboardButton(text = 'Связь с менеджером', callback_data = 'communication')]
 ])
 
+# клавиатура каталог
+catalog = InlineKeyboardMarkup(inline_keyboard = [
+    [InlineKeyboardButton(text = 'Карт1', callback_data = 'cart1')],
+    [InlineKeyboardButton(text = 'Карт2', callback_data = 'cart2')],
+    [InlineKeyboardButton(text = 'Карт3', callback_data = 'cart3')],
+    [InlineKeyboardButton(text = 'Карт4', callback_data = 'cart4')],
+    [InlineKeyboardButton(text = 'Назад', callback_data = 'back_price')]
+])
 
-back = InlineKeyboardMarkup(inline_keyboard = [
-    [InlineKeyboardButton(text = 'Назад', callback_data = 'back')]
+# инфо
+info = InlineKeyboardMarkup(inline_keyboard = [
+    [InlineKeyboardButton(text = 'Назад', callback_data = 'back_info')]
+])
+
+# клавиатура отзывы
+feedback = InlineKeyboardMarkup(inline_keyboard = [
+    [InlineKeyboardButton(text = 'Перейти в канал', url = "https://t.me/foma_test")],
+    [InlineKeyboardButton(text = 'Назад', callback_data = 'back_feedback')]
+])
+
+# обычная клавиатура для связи с менеджером
+communication = InlineKeyboardMarkup(inline_keyboard = [
+    [InlineKeyboardButton(text = 'WhatsApp', url = f"https://wa.me/{88005553535}")],
+    [InlineKeyboardButton(text = 'Telegram', url = 'https://t.me/username')],
+    [InlineKeyboardButton(text = 'Назад', callback_data = 'back_communication')]
+])
+
+# клавиатура 'связь с менеджером' для заказа
+communication_order = InlineKeyboardMarkup(inline_keyboard = [
+    [InlineKeyboardButton(text = 'WhatsApp', url = f"https://wa.me/{88005553535}")],
+    [InlineKeyboardButton(text = 'Telegram', url = 'https://t.me/username')],
+    [InlineKeyboardButton(text = 'Назад', callback_data = 'back_communication_order')]
+])
+
+# клавиатура карт1
+cart_order = InlineKeyboardMarkup(inline_keyboard = [
+    [InlineKeyboardButton(text = 'Записаться', callback_data = 'order')],
+    [InlineKeyboardButton(text = 'Связь с менеджером', callback_data = 'communication_order')],
+    [InlineKeyboardButton(text = 'Выбрать другой картинг', callback_data = 'back_cart_order')]
 ])
