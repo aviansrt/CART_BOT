3057648eda978ff1803e68fecdd7db1e3fea45e7
Simon <168690442+aviansrt@users.noreply.github.com>
2025-08-07 21:00:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import hashlib
 
 # получение токена из переменной окружения с запасным значением
-TOKEN = os.getenv("BOT_TOKEN", "7705262778:AAEkkVZTLm-BhqfzKgzBMuzqRa5ldy27a-Q")
+TOKEN = os.getenv("BOT_TOKEN", "токен бота")
 TARGET_CHAT = os.getenv("TARGET_CHAT", "-1002581159121")
 MAX_LENGTH = 200
 
@@ -15,4 +15,5 @@ hash_object = hashlib.sha256(TOKEN.encode())
 hashed_token = hash_object.hexdigest()
 
 # вывод для отладки
+
 print("Захешированный токен:", hashed_token)
